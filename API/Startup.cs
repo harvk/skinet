f45359df11d12c80f79bee4d3c74479737d05565
King P <k.harvey324@yahoo.com>
2023-04-11 23:53:34 -0400

    Added pagination, sorting, filtering, search, and CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,15 @@ namespace API
             // user-defined extension method call
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+
+            // allows access control from other origins
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +65,8 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
